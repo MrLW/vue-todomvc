01c1f1483a33080793731bdc6f3a76741d080a0e
lingxi <lingxi@jfjun.com>
2019-01-20 17:54:32 +0800

    初始化todo项目结构

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './components/app.vue'
+const root = document.createElement('div')
+document.body.appendChild(root)
+
+new Vue({
+    render: (h) => h(App)
+}).$mount(root);
